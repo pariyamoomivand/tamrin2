bb68fec7152d3386c38f72415ecbca7915c66f6b
Pariya Moomivand <moomivandpariya4@gmail.com>
2025-11-17 12:14:57 +0330

    pages creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PublicTemplate } from './+pages/+public/public-template/public-template';
+import { LoginPage } from './+pages/+public/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'public',component:PublicTemplate,children:[
+        {path:'login',component:LoginPage},
+        {path:'',redirectTo:'login',pathMatch:'prefix'},
+    ]},
+    {path:'', redirectTo:'public',pathMatch:'full'},
+];

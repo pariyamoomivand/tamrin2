579a88a6668e563d988a7ab95753731739d4447a
Pariya Moomivand <moomivandpariya4@gmail.com>
2025-11-17 15:15:49 +0330

    creation menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { PublicTemplate } from './+pages/+public/public-template/public-template';
 import { LoginPage } from './+pages/+public/login-page/login-page';
+import { HomePage } from './+pages/+public/home-page/home-page';
 
 export const routes: Routes = [
     {path:'public',component:PublicTemplate,children:[
+        {path:'home',component:HomePage},
         {path:'login',component:LoginPage},
-        {path:'',redirectTo:'login',pathMatch:'prefix'},
+        {path:'',redirectTo:'home',pathMatch:'prefix'},
     ]},
     {path:'', redirectTo:'public',pathMatch:'full'},
 ];
